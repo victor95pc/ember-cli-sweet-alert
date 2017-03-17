ebedafb36237afb8eb9522c563e2f51375338dcd
Victor Palomo de Castro <victorpalomocastro@gmail.com>
2017-03-17 08:43:57 -0300

    Upgrade sweetalert to version 2

--- blueprints/ember-cli-sweet-alert/index.js
+++ blueprints/ember-cli-sweet-alert/index.js
@@ -1,11 +1,7 @@
 module.exports = {
-  normalizeEntityName: function() {
-    // allows us to run ember -g ember-cli-bootstrap and not blow up
-    // because ember cli normally expects the format
-    // ember generate <entitiyName> <blueprint>
-  },
+  normalizeEntityName: function() {},
 
   afterInstall: function () {
-    return this.addBowerPackageToProject('sweetalert', '~1.1.0');
+    return this.addBowerPackageToProject('sweetalert2', '~6');
   }
 };
